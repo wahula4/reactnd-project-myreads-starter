cd11c063f400af67c1b62c419d8ea0e28a165786
Tony Wahula <mawahula4@gmail.com>
2018-11-19 20:17:39 -0700

    refactoring components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
-import React from 'react'
-// import * as BooksAPI from './BooksAPI'
+import React, { Component } from 'react'
 import './App.css'
 import { Route } from 'react-router-dom'
-import ListBooks from './ListBooks'
 import SearchBooks from './SearchBooks'
+import ListBooks from './ListBooks'
 
-class BooksApp extends React.Component {
+class BooksApp extends Component {
 
   render() {
     return (
       <div className="app">
-      <Route exact path='/' render ={() => (
-        <ListBooks />
-      )} />
+      <Route exact path='/' component ={ListBooks} />
       <Route path='/search' render ={() => (
         <SearchBooks />
       )} />
