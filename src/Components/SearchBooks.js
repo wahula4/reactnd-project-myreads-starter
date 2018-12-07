824c455200c8d3142f247d65b9bb5c22e0ba4de5
Tony Wahula <mawahula4@gmail.com>
2018-12-06 21:35:46 -0700

    error handling for search results

--- src/Components/SearchBooks.js
+++ src/Components/SearchBooks.js
@@ -20,10 +20,11 @@ class SearchBooks extends Component {
    async componentDidMount() {
     const books = await BooksAPI.getAll()
     this.setState( { books } )
-}
+  }
 
     // value typed into search bar will be set as the state for query
     handleChange = async e => {
+      // try{
       const query = e.target.value
       this.setState( { query } )
       
@@ -36,18 +37,23 @@ class SearchBooks extends Component {
       else {
             // Returns a Promise which resolves to a JSON object containing a collection of a maximum of 20 book objects
             // if a query exists, search that query and set the state of books to be equal to the books that pass the search
-            const results = await BooksAPI.search(query)
-            // if the id of the results match the id of a shelved book, set the shelf value of the searched book
-            for(let result of results){
-              result.shelf = 'none'
-              for(let book of this.state.books){
-                if(book.id === result.id){
-                  result.shelf = book.shelf
-                }
-              }
-            }
-            this.setState( { results } )
-           }
+            await BooksAPI.search(query).then(results => {
+              if(!results || results.hasOwnProperty('error')) {
+                this.setState({ results: [] })
+              } else {
+                // if the id of the results match the id of a shelved book, set the shelf value of the searched book
+                  for(let result of results){
+                    result.shelf = 'none'
+                    for(let book of this.state.books){
+                      if(book.id === result.id){
+                        result.shelf = book.shelf
+                      }
+                    }
+                  }
+                  this.setState( { results } )
+                }  
+            })
+           }      
       }
 
     render() {
@@ -67,15 +73,16 @@ class SearchBooks extends Component {
                        />
               </div>
             </div>
-            <div className="search-books-results">
+            <div className="search-books-results" id="hello">
               <ol className="books-grid">
-                {results.length > 0 && results.map(book => 
+                {results.length > 0 ? results.map(book => 
                     <Book
                       book={book}
                       key={book.id}
                       updateShelf={updateShelf}
                     />
-                  )}
+                  ) : "No Results"
+                }
               </ol>
             </div>
           </div>
